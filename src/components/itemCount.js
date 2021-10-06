ff9cf5f03e6e3127a3be4844a4bcb9087e2e1e67
gonzalo morel cortes <morelcortesgonzalo@gmail.com>
2021-10-06 20:51:25 -0300

    agregue onAdd

--- src/components/itemCount.js
+++ src/components/itemCount.js
@@ -1,35 +1,46 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import "./itemCount.css";
 import {Button} from 'react-bootstrap';
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const ItemCount = ({stock=5, cantidad=1}) => {
-    const [counter, setCounter] = useState(cantidad);
+const ItemCount = ({stock=7, initial=1, onAdd}) => {
+    const [counter, setCounter] = useState(initial);
+    const isInitialMount = useRef(true);
 
-    const handleIncrement = () => {
+    const increment = () => {      // donde subo la cantidad de elementos a comprar
         if (stock > counter){ 
             setCounter(counter + 1);
+            console.log(counter)
         }else {
-            console.log ("no hay mas stock")
+            console.log ("llego al limite del stock disponible")
         };
     };
     
-    
-    const handleDecrement = () => {
-        if (counter > cantidad){
+    const decrement = () => {      // donde bajo la cantidad de elementos a comprar
+        if (counter > initial){
             setCounter(counter - 1);
         };
     };
 
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            onAdd(counter)
+        }
+    })
+
+
     return (
         <div className="col-xs-6">
             <div className="product_quantity">
-                <Button onClick={handleDecrement} className="me-auto" variant="danger" size="sm"><FontAwesomeIcon icon={faMinus}/></Button> 
+                <Button onClick={decrement} className="me-auto" variant="danger" size="sm"><FontAwesomeIcon icon={faMinus}/></Button> 
                 <span className= "ms-3 me-2">Cantidad: {counter}   </span>
                 {/* <input className="prod-input" type="text" value="1"/> */}
-                <Button onClick={handleIncrement} variant="success" size="sm"><FontAwesomeIcon icon={faPlus}/></Button>
+                <Button onClick={increment} variant="success" size="sm"><FontAwesomeIcon icon={faPlus}/></Button>
             </div>
+            {/* <Button variant="primary">Agregar al carrito</Button> */}
         </div>   
     )
 }
